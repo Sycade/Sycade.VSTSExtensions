3cc757b2500c03e9db8f77bfd5ad82c39b6d45ae
Michiel Lowijs <michiel.lowijs@sycade.com>
2016-01-30 22:24:41 +0100

    Added files

---
+++ PatchAssemblyInfo/PatchAssemblyInfo.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param (
+    [string]$asmVersion
+)
+
+Write-Host "Starting PatchAssemblyInfo"
+Write-Host "asmVersion = $asmVersion"
+
+$asmFiles = Get-ChildItem -Filter "*AssemblyInfo.cs" -Recurse
+
+$asmFiles | % {
+    Write-Host ("Patching file '{0}'" -f $_.FullName)
+
+    $content = Get-Content $_.FullName
+
+    $content = $content -replace '(Assembly.+?Version)\(".+"\)\]', ('$1("{0}")]' -f $asmVersion)
+
+    Set-Content $_.FullName $content
+}
+
+Write-Host "Finished PatchAssemblyInfo"
